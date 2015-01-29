0f1612365c06a47f00e8eb6ed76d2abc78ac9d7b
Tranel Bland <tranelb@gmail.com>
2015-01-29 02:44:13 +0000

    another try

---
+++ pbar01.py
@@ -0,0 +1,24 @@
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
+#def bartender ():
+    
+for item in questions:
+    #raw_input = ("do you like % ?")%s(x)
+    drink = []
+    raw_input = ("do you like a %s drink?") % item
+    if item == "yes":
+        drink.append()
